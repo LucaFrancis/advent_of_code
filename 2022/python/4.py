056c99154123f14c8db2e48067a8ee4f35fe76d9
Luca <luca.francis@ninjaneers.de>
2022-12-04 09:14:29 +0100

    Python 4.2

--- 2022/python/4.py
+++ 2022/python/4.py
@@ -5,6 +5,9 @@ def is_fully_contained(s1_start, s1_end, s2_start, s2_end):
     return s1_set.issubset(s2_set) or s2_set.issubset(s1_set)
 
 
+def is_overlap(s1_start, s1_end, s2_start, s2_end):
+    return s1_start <= s2_end and s2_start <= s1_end
+
 if __name__ == '__main__':
     f = open("../inputs/4/input", "r")
     lines = f.readlines()
@@ -13,6 +16,6 @@ if __name__ == '__main__':
         section1, section2 = line.split(",")
         s1_start, s1_end = list(map(lambda s: int(s), section1.split("-")))
         s2_start, s2_end = list(map(lambda s: int(s), section2.split("-")))
-        if is_fully_contained(s1_start, s1_end, s2_start, s2_end):
+        if is_overlap(s1_start, s1_end, s2_start, s2_end):
             score += 1
     print(score)
